37f5d1e469c16133bce65199861cae126af17fdc
Sergii Bielousov <sergii.bielousov@synapse.com>
2018-05-14 15:17:52 +0300

    Initial commit

---
+++ src/test/java/TheTestOne.java
@@ -0,0 +1,27 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TheTestOne {
+
+
+
+
+        @Test
+        public void firstTest() {
+//            ChromeDriverManager.getInstance().setup();
+            FirefoxDriverManager.getInstance().setup();
+
+            WebDriver driver = new FirefoxDriver();
+            driver.get("http://google.com");
+
+            driver.quit();
+
+
+            System.out.println("test ");
+        }
+    }
+
