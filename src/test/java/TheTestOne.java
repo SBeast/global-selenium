7180282f923fec34efdb833952362cc369f42508
sergii.bielousov <sbeast2003@gmail.com>
2018-05-14 16:42:59 +0300

    Second coommit

--- src/test/java/TheTestOne.java
+++ src/test/java/TheTestOne.java
@@ -17,6 +17,7 @@ public class TheTestOne {
 
             WebDriver driver = new ChromeDriver();
 //            WebDriver driver = new FirefoxDriver();
+            //            WebDriver driver = new FirefoxDriver();
             driver.get("http://google.com");
 
             driver.quit();
