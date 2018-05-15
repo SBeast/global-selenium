82af511fac01d5a14287cbc288bfadf9a3cbe5e2
sergii.bielousov <sbeast2003@gmail.com>
2018-05-15 16:45:09 +0300

    Second coommit

--- src/test/java/TheTestOne.java
+++ src/test/java/TheTestOne.java
@@ -1,9 +1,13 @@
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.EdgeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class TheTestOne {
 
@@ -14,14 +18,18 @@ public class TheTestOne {
         public void firstTest() {
             ChromeDriverManager.getInstance().setup();
 //            FirefoxDriverManager.getInstance().setup();
+//            InternetExplorerDriverManager.getInstance().setup();
+//            EdgeDriverManager.getInstance().setup();
 
             WebDriver driver = new ChromeDriver();
 //            WebDriver driver = new FirefoxDriver();
-            
-            driver.get("http://localhost/litecart/");
+//            WebDriver driver = new InternetExplorerDriver();
+//            WebDriver driver = new EdgeDriver();
+
+//            driver.get("http://localhost/litecart/");
+            driver.get("http://news.google.com.ua");
 
             driver.quit();
-
 
             System.out.println("test 2");
         }
